dc3e52b8bbba1cf9b67328f40c443b8a2728548e
OlegStavchanskiy <olegswork@mail.ru>
2023-11-12 21:22:45 +0300

    lab6

--- src/main/java/ch/makery/lab4/MainApp.java
+++ src/main/java/ch/makery/lab4/MainApp.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -34,7 +35,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Преподаватели");
-
+        this.primaryStage.getIcons().add(new Image("C:/Users/Xenon/IdeaProjects/lab4/src/main/resources/ch/makery/lab4/9349828.png"));
         initRootLayout();
 
         showPersonOverview();
